2175e8cdac066c4ca92f5ec19a21d55f1be858c3
Jan Meier <jan@meier.wtf>
2017-03-30 17:15:01 +0200

    CircleVectorIcon, Sample; improved api & sample

--- sample/src/main/java/wtf/meier/circledvectoriconexample/MainActivity.java
+++ sample/src/main/java/wtf/meier/circledvectoriconexample/MainActivity.java
@@ -1,15 +1,30 @@
 package wtf.meier.circledvectoriconexample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-import wtf.meier.circledvectoriconexample.R;
+import wtf.meier.circledvectoricon.CircledVectorIcon;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    boolean isColorToggle = false;
+
+    private CircledVectorIcon circleVectorIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.circleVectorIcon = (CircledVectorIcon) findViewById(R.id.circleVectorIcon);
+        circleVectorIcon.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        circleVectorIcon.setDrawableColor(isColorToggle ? R.color.colorPrimary : R.color.colorAccent);
+        circleVectorIcon.setCircleColor(isColorToggle ? R.color.colorAccent : R.color.colorPrimary);
+        isColorToggle = !isColorToggle;
     }
 }
