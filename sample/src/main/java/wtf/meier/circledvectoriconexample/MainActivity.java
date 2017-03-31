f152ad0a4ba08f406b742d9ef79421fcb7222308
Jan Meier <jan@meier.wtf>
2017-03-31 19:25:19 +0200

    Sample; added SeekBar to visualize padding changability

--- sample/src/main/java/wtf/meier/circledvectoriconexample/MainActivity.java
+++ sample/src/main/java/wtf/meier/circledvectoriconexample/MainActivity.java
@@ -2,23 +2,40 @@ package wtf.meier.circledvectoriconexample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import wtf.meier.circledvectoricon.CircledVectorIcon;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, SeekBar.OnSeekBarChangeListener {
 
     boolean isColorToggle = false;
 
+    private TextView currentValueDisplayTextView;
     private CircledVectorIcon circleVectorIcon;
+    private SeekBar paddingSeekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final float initialPercentageFloat = CircledVectorIcon.DEFAULT_PADDING_IN_PERCENT;
+        final int initialPercentageInt = (int) (initialPercentageFloat * 100);
+
         this.circleVectorIcon = (CircledVectorIcon) findViewById(R.id.circleVectorIcon);
         circleVectorIcon.setOnClickListener(this);
+
+        this.currentValueDisplayTextView = (TextView) findViewById(R.id.currentValueDisplay);
+        currentValueDisplayTextView.setText(getString(R.string.current_percentage_value_display, initialPercentageInt, initialPercentageFloat));
+
+        this.paddingSeekBar = (SeekBar) findViewById(R.id.imagePaddingSeekBar);
+        paddingSeekBar.setProgress(initialPercentageInt);
+        paddingSeekBar.setOnSeekBarChangeListener(this);
+        paddingSeekBar.setMax(50);
+
     }
 
     @Override
@@ -26,5 +43,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         circleVectorIcon.setDrawableColor(isColorToggle ? R.color.colorPrimary : R.color.colorAccent);
         circleVectorIcon.setCircleColor(isColorToggle ? R.color.colorAccent : R.color.colorPrimary);
         isColorToggle = !isColorToggle;
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        float newProgress = progress / 100f;
+        Log.d("MainActivity", String.format("new padding: %f", newProgress));
+        circleVectorIcon.setImageSidePaddingInPercent(newProgress);
+        currentValueDisplayTextView.setText(getString(R.string.current_percentage_value_display, progress, newProgress));
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
     }
 }
